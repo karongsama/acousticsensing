93a094896390b3ed6dd5b6979a306f73129a2eeb
karongsama <115446665+karongsama@users.noreply.github.com>
2023-10-06 14:34:39 +0800

    phasenetpick

---
+++ Draw/NewCylinder/ctrebuild.py
@@ -0,0 +1,91 @@
+import time
+import numpy as np
+from skimage import io
+import plotly.graph_objects as go
+import os
+
+path=r"D:\coding\AcousticSensing\Data\NewCylinder\CTpic\ctpicture_up"
+vol=tif_collection = io.imread(os.path.join(path, '*.tif'))
+volume=vol.T
+r,c=volume[0].shape
+# print(volume.shape)
+frames=375
+
+fig = go.Figure(frames=[go.Frame(data=go.Surface(
+    z=(37.4 - k * 0.1) * np.ones((r, c)),
+    surfacecolor=np.flipud(volume[frames - 1 - k]),
+    cmin=0, cmax=200
+    ),
+    name=str(k) # you need to name the frame for the animation to behave properly
+    )
+    for k in range(frames)
+    ]
+)
+
+fig.add_trace(go.Surface(
+    z=37.4 * np.ones((r, c)),
+    surfacecolor=np.flipud(volume[frames-1]),
+    colorscale='Gray',
+    cmin=0, cmax=200,
+    colorbar=dict(thickness=20, ticklen=4)
+))
+
+def frame_args(duration):
+    return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
+
+sliders = [
+            {
+                "pad": {"b": 10, "t": 60},
+                "len": 0.9,
+                "x": 0.1,
+                "y": 0,
+                "steps": [
+                    {
+                        "args": [[f.name], frame_args(0)],
+                        "label": str(k),
+                        "method": "animate",
+                    }
+                    for k, f in enumerate(fig.frames)
+                ],
+            }
+        ]
+
+# Layout
+fig.update_layout(
+         title='Slices in volumetric data',
+         width=2000,
+         height=2000,
+         scene=dict(
+                    zaxis=dict(range=[-0.1, 37.5], autorange=False),
+                    aspectratio=dict(x=1, y=1, z=1),
+                    ),
+         updatemenus = [
+            {
+                "buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+)
+
+fig.show()
